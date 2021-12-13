5765decb4e17bacd8a64d34b8d147963e3c263cb
dyatelok <gamovoleksii@gmail.com>
2021-12-13 12:07:33 +0200

    code style

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,6 @@ mod ss;
 use crate::ss::ascii_render::*;
 
 fn main() {
-    let mut ascii_render : Play = Play::new(132,42);
+    let mut ascii_render: Play = Play::new(132, 42);
     ascii_render.run();
 }
